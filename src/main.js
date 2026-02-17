54202f0a26670e314f91586b149a9736b0cca19c
Shreyasi <poddarshreyasi@gmail.com>
2026-02-16 22:33:50 -0500

    todos-08: Implement todo filtering

--- src/main.js
+++ src/main.js
@@ -14,9 +14,22 @@ function renderTodos() {
   const todoListElement = document.getElementById("todo-list");
   todoListElement.innerHTML = ""; // clear the current list
 
-  // Loop through the filtered todos and add them to the DOM
+  // Filter todos based on the current filter setting
+  let filteredTodos = [];
   for (let i = 0; i < todos.length; i++) {
     const todo = todos[i];
+    if (filter === "all") {
+      filteredTodos.push(todo);
+    } else if (filter === "completed" && todo.completed) {
+      filteredTodos.push(todo);
+    } else if (filter === "active" && !todo.completed) {
+      filteredTodos.push(todo);
+    }
+  }
+
+  // Loop through the filtered todos and add them to the DOM
+  for (let i = 0; i < filteredTodos.length; i++) {
+    const todo = filteredTodos[i];
 
     const todoItem = document.createElement("div");
     todoItem.classList.add("p-4", "todo-item");
@@ -53,3 +66,41 @@ function handleNewTodoKeyDown(event) {
 
 const newTodoInput = document.getElementById("new-todo");
 newTodoInput.addEventListener("keydown", handleNewTodoKeyDown);
+
+// Function to handle filter selection from the navbar
+function handleClickOnNavbar(event) {
+  // if the clicked element is an anchor tag
+  if (event.target.tagName === "A") {
+    const hrefValue = event.target.href;
+    const action = hrefValue.split("/").pop();
+    filter = action === "" ? "all" : action;
+    renderTodos();
+    renderTodoNavBar(hrefValue);
+  }
+}
+
+const todoNav = document.getElementById("todo-nav");
+todoNav.addEventListener("click", handleClickOnNavbar);
+
+// Function to update the navbar anchor elements
+function renderTodoNavBar(href) {
+  const elements = todoNav.children;
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    if (element.href === href) {
+      element.classList.add(
+        "underline",
+        "underline-offset-4",
+        "decoration-rose-800",
+        "decoration-2"
+      );
+    } else {
+      element.classList.remove(
+        "underline",
+        "underline-offset-4",
+        "decoration-rose-800",
+        "decoration-2"
+      );
+    }
+  }
+}
