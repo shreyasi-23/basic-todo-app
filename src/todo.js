d450431a101de58748dca471f8e9cdf947724e32
Shreyasi <poddarshreyasi@gmail.com>
2026-02-19 00:08:29 -0500

    todos-16: Add private fields and getters/setters

--- src/todo.js
+++ src/todo.js
@@ -1,22 +1,50 @@
 class Todo {
+  #id
+  #text
+  #completed
+
   constructor(id, text, completed = false) {
-    this.id = id;
-    this.text = text;
-    this.completed = completed;
+    this.#id = id;
+    this.#text = text;
+    this.#completed = completed;
   }
 
   toggle() {
-    this.completed = !this.completed;
+    this.#completed = !this.#completed;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get text() {
+    return this.#text;
+  }
+
+  get completed() {
+    return this.#completed;
+  }
+
+  set text(newText) {
+    this.#text = newText;
+  }
+
+  set completed(value) {
+    this.#completed = value;
   }
 }
 
 const todo = new Todo(1, "Buy milk");
-todo.toggle();
-
+console.log(todo);
 console.log(todo.id);
 console.log(todo.text);
 console.log(todo.completed);
 
-todo.id = 2;
+// todo.id = 2;
+todo.text = "Buy eggs";
+todo.completed = true;
 
 console.log(todo);
+console.log(todo.id);
+console.log(todo.text);
+console.log(todo.completed);
